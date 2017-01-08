6c19dab692e57df67035924f92d0c98081dbb0f5
github-ojha <nkojha@gmaill.com>
2017-01-08 19:24:58 +0530

    registered and injected ProductService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.css';
+import {ProductService} from '../../src/app/products/product.service'
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers : [ProductService]
 })
 export class AppComponent { }
